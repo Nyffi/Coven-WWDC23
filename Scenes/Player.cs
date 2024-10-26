e4c182e3a6a769aa40cc9e6825e24a1d4a7e68cb
Paulo Rocha <pcrochadev@gmail.com>
2024-10-26 04:12:04 -0300

    Player movement implemented

---
+++ Scenes/Player.cs
@@ -0,0 +1,47 @@
+using Godot;
+
+namespace CovenWWDC23Port.Scenes;
+
+public partial class Player : Node2D
+{
+
+	private bool _invulnerable; 
+	private void UpdatePlayerPosition()
+	{
+		Vector2 mousePos = GetViewport().GetMousePosition();
+		
+		if (mousePos.X < 20)
+			mousePos.X = 20;
+		else if (mousePos.X > 700)
+			mousePos.X = 700;
+
+		if (mousePos.Y < 25)
+			mousePos.Y = 25;
+		else if (mousePos.Y > 935)
+			mousePos.Y = 935;
+		
+		//GD.Print($"Pos: {mousePos}");
+		Position = mousePos;
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		UpdatePlayerPosition();
+	}
+
+	private void OnDamage(Area2D area)
+	{
+		GD.Print("Player took damage");
+	}
+	
+	private void OnGraze(Area2D area)
+	{
+		GD.Print("Player grazed a projectile");
+	}
+}
